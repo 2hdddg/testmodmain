71d888aebe0b50e9e6f55c43ef8d4883cfbdcd69
Peter Wilhelmsson <2hdddg@gmail.com>
2019-04-13 14:15:26 +0200

    Initial

---
+++ go.mod
@@ -0,0 +1,5 @@
+module mod
+
+require github.com/2hdddg/testmod v1.0.1
+
+go 1.12
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/2hdddg/testmod"
+)
+
+func main() {
+	fmt.Println(testmod.Hi("vim-go"))
+}
